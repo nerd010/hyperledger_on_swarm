91ff2467cef33697e9762487f29ce5888c11a64e
chaibaoyang <baoyangchai@gmail.com>
2017-09-29 12:02:53 +0800

    update dir

--- genConfig/build.sh
+++ genConfig/build.sh
@@ -4,6 +4,8 @@ wkdir=$(dirname -- $0)
 cd $wkdir;
 PWD=`pwd`
 docker run --rm -v "$PWD":/usr/src/myapp -v "/Users/baoyangc/code/goprojs":/gopath -w /usr/src/myapp -e GOPATH=/gopath go1.9 go build
+mkdir -p ../bin/linux-amd64
 mv myapp ../bin/linux-amd64/genConfig
 go build
+mkdir -p ../bin/darwin-amd64
 cp genConfig ../bin/darwin-amd64/genConfig
